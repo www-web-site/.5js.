d226275335b0a7fe5df9d4afdf48ed73240ecf35
www-web-site <329blogertop228@gmail.com>
2025-04-24 16:52:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const askName = prompt('Ismingiz:');
+const askId = prompt('Space ID:');
+const askPass = prompt('Space Parol:');
+const info = {
+    name: askName,
+    id: askId,
+    password: askPass,
+}
+
+console.log(info);
